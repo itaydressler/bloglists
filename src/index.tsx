8dc12df84c143f4df6384cac20315b58d89d72ed
Itay Dressler <itaydressler@gmail.com>
2017-05-07 23:04:21 +0300

    Page view test.

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as ReactGA from 'react-ga';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 
-const ReactGA = require('react-ga');
 ReactGA.initialize('UA-98695951-1');
+ReactGA.pageview('/main');
